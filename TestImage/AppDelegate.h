1a151820cbad814de3cc06b5cdebf0ebe05f0960
Zhu Yu <mike.zhuyu@gmail.com>
2017-06-06 16:34:00 +0800

    Initial Commit

---
+++ TestImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestImage
+//
+//  Created by Zhu Yu on 2017/6/6.
+//  Copyright © 2017年 Zhu Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestImage
+//
+//  Created by Zhu Yu on 2017/6/6.
+//  Copyright © 2017年 Zhu Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
